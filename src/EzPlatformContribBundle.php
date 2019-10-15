b3548b876feef2a9345b38c53e558dcd21658000
Andrew Longosz <alongosz@users.noreply.github.com>
2019-10-15 19:10:27 +0200

    [Symfony] Created EzPlatformContribBundle class

---
+++ src/EzPlatformContribBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Andrew W. Longosz.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace AWLSolutions\EzPlatformContribBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class EzPlatformContribBundle extends Bundle
+{
+}
